90a4f42d3acdf5efc9983cac9db243c6be5c49e8
lhbzr <luis@dotcore.com.br>
2016-02-10 16:53:40 -0200

    Small changes in loader and music.

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,47 @@
 import get from './lib/get'
-
 import Music from './music'
 import Scene from './scene'
 
+/**
+ * About.
+ */
 import about from './about'
-import menu from './menu'
 
 about()
+
+/**
+ * Menu.
+ */
+import menu from './menu'
+
 menu()
 
+/**
+ * Music.
+ */
 const music = new Music()
 const musicPrev = document.querySelector('.music-prev')
 const musicToggle = document.querySelector('.music-toggle')
 const musicNext = document.querySelector('.music-next')
 
+music.audio.addEventListener('pause', () => {
+  musicToggle.classList.add('is-paused')
+})
+
+music.audio.addEventListener('play', () => {
+  musicToggle.classList.remove('is-paused')
+})
+
+music.audio.addEventListener('ended', () => {
+  music.load((music.song < music.songs.length - 1) ? music.song + 1 : 0)
+})
+
 musicToggle.addEventListener('click', (e) => {
   e.stopPropagation()
 
   if (music.isPaused()) {
-    musicToggle.classList.remove('is-paused')
-
     music.play()
   } else {
-    musicToggle.classList.add('is-paused')
-
     music.pause()
   }
 })
@@ -31,19 +49,18 @@ musicToggle.addEventListener('click', (e) => {
 musicPrev.addEventListener('click', (e) => {
   e.stopPropagation()
 
-  musicToggle.classList.remove('is-paused')
-
   music.prev()
 })
 
 musicNext.addEventListener('click', (e) => {
   e.stopPropagation()
 
-  musicToggle.classList.remove('is-paused')
-
   music.next()
 })
 
+/**
+ * Scene.
+ */
 const scene = new Scene(music)
 
 scene.createGeometry()
@@ -51,6 +68,9 @@ scene.createLight()
 scene.createShaders()
 scene.render()
 
+/**
+ * SVG.
+ */
 get('dist/svg/svg.svg', (res) => {
   const body = document.body
   const wrapper = document.createElement('div')
@@ -61,6 +81,9 @@ get('dist/svg/svg.svg', (res) => {
   body.insertBefore(wrapper, body.childNodes[0])
 })
 
+/**
+ * Window.
+ */
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader')
 
@@ -69,12 +92,10 @@ window.addEventListener('load', () => {
   const timeline = new TimelineMax()
 
   timeline
-    .fromTo('.body-border', 0.5, { scale: 0 }, { scale: 1 })
-    .fromTo('.body-dots', 0.5, { autoAlpha: 0 }, { autoAlpha: 1 })
-    .fromTo('.about', 0.5, { autoAlpha: 0 }, { autoAlpha: 1 })
-    .fromTo('.music, .share', 0.5, { autoAlpha: 0 }, { autoAlpha: 1 })
-    .staggerFromTo('.menu-link', 0.25, { autoAlpha: 0 }, { autoAlpha: 1 }, 0.25)
-    .fromTo('.canvas', 0.5, { autoAlpha: 0 }, { autoAlpha: 1 })
+    .fromTo('.about', 1, { autoAlpha: 0 }, { autoAlpha: 1 })
+    .fromTo('.music, .share', 1, { autoAlpha: 0 }, { autoAlpha: 1 })
+    .staggerFromTo('.menu-link', 1, { autoAlpha: 0 }, { autoAlpha: 1 }, 0.25)
+    .fromTo('.canvas', 1, { autoAlpha: 0 }, { autoAlpha: 1 })
 })
 
 window.addEventListener('resize', (e) => {
