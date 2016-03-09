fdab038eac99bb378466c9cda5dd906b5e25319e
lhbzr <luis@dotcore.com.br>
2016-03-09 01:30:38 -0300

    Changes in experiments.

--- experiments/deform/index.js
+++ experiments/deform/index.js
@@ -11,27 +11,28 @@ const Analyser = require('web-audio-analyser')
 const OrbitControls = require('three-orbit-controls')(THREE)
 const glslify = require('glslify')
 
+const EffectComposer = require('three-effectcomposer')(THREE)
+const DotScreenShader = require('./shaders/dotScreenShader')
+const RGBShiftShader = require('./shaders/rgbShift')
+
 class Paralyzed {
   constructor () {
-    this.color = '#FFAA00'
-
+    this.color = '#5A00FF'
     this.gui = null
-
     this.renderer = null
     this.scene = null
     this.camera = null
-
+    this.composer = null
     this.controls = null
+    this.clock = new THREE.Clock({ autoStart: true })
 
     this.loadSong()
-
     this.createRender()
     this.createScene()
     this.createGeometry()
-
+    this.createShaders()
     this.startGUI()
     this.startStats()
-
     this.update()
   }
 
@@ -41,16 +42,17 @@ class Paralyzed {
     const _this = this
 
     this.gui.addColor(this, 'color').onChange(function () {
-      console.log('Not figured out how to make this work.')
+      _this.mesh.material.uniforms.u_background.value = new THREE.Color(_this.color)
     })
   }
 
   startStats () {
     this.stats = new Stats()
+
+    this.stats.domElement.style.bottom = 0
     this.stats.domElement.style.display = 'block'
     this.stats.domElement.style.position = 'absolute'
     this.stats.domElement.style.right = 0
-    this.stats.domElement.style.top = 0
     this.stats.domElement.style.zIndex = 50
 
     document.body.appendChild(this.stats.domElement)
@@ -97,7 +99,7 @@ class Paralyzed {
         }},
         u_speed: { type: 'f', value: 0.5 },
         u_color: { type: 'c', value: new THREE.Color(0xFFFFFF) },
-        u_background: { type: 'c', value: new THREE.Color(0xFFAA00) },
+        u_background: { type: 'c', value: new THREE.Color(this.color) },
         u_brightness: { type: 'f', value: 1 }
       }
     })
@@ -107,6 +109,24 @@ class Paralyzed {
     this.scene.add(this.mesh)
   }
 
+  createShaders () {
+    this.scene.add(new THREE.AmbientLight(0xFF00FF))
+
+    this.composer = new EffectComposer(this.renderer)
+    this.composer.addPass(new EffectComposer.RenderPass(this.scene, this.camera))
+
+    const dotEffect = new EffectComposer.ShaderPass(DotScreenShader)
+    dotEffect.uniforms.scale.value = 4
+
+    this.composer.addPass(dotEffect)
+
+    const rgbEffect = new EffectComposer.ShaderPass(RGBShiftShader)
+    rgbEffect.uniforms.amount.value = 0.05
+    rgbEffect.renderToScreen = true
+
+    this.composer.addPass(rgbEffect)
+  }
+
   loadSong () {
     const AudioContext = window.AudioContext || window.webkitAudioContext
 
@@ -114,14 +134,14 @@ class Paralyzed {
 
     Soundcloud({
       client_id: '78c6552c14b382e23be3bce2fc411a82',
-      song: 'https://soundcloud.com/mysteryskulls/paralyzed-single-version',
+      song: 'https://soundcloud.com/iamtheelephante/clean-bandit-rather-be-elephante-remix',
       dark: false,
       getFonts: false
     }, (err, src, data, div) => {
       if (err) throw err
 
       _this.music = new Audio()
-      _this.music.crossOrigin = 'Anonymous'
+      _this.music.crossOrigin = 'anonymous'
       _this.music.src = src
       _this.music.play()
 
@@ -135,26 +155,29 @@ class Paralyzed {
   update () {
     this.stats.begin()
 
+    this.composer.render()
     this.controls.update()
 
     const array = this.musicAnalyser ? this.musicAnalyser.frequencies() : [1]
     const arrayAverage = array.reduce((p, c) => p + c, 0) / array.length
 
+    const clockElapsedTime = this.clock.getElapsedTime()
+
     GSAP.to(this.material.uniforms.u_brightness, 0.2, {
-      ease: Power0.easeNone,
+      // ease: Power0.easeNone,
       value: arrayAverage / 50
     })
 
     GSAP.to(this.material.uniforms.u_time, 0.2, {
       ease: Power0.easeNone,
-      value: `+= ${arrayAverage / 1000}`
+      value: clockElapsedTime / 2
     })
-
-    this.renderer.render(this.scene, this.camera)
 
     this.stats.end()
 
     requestAnimationFrame(this.update.bind(this))
+
+    this.renderer.render(this.scene, this.camera)
   }
 }
 
